efc8471624c8e3ddd254bede801fa1ce687462ec
TalitaBorges <talitaramosborges.arte@gmail.com>
2020-12-16 14:31:31 +0000

    Implementacao do form contactos

---
+++ GlobalGamesCet49/Dados/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using GlobalGamesCet49.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCet49.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<ContactoOrcamento> ContactoOrcamentos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ GlobalGamesCet49/Dados/Entidades/ContactoOrcamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCet49.Dados.Entidades
+{
+    public class ContactoOrcamento
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mensagem { get; set; }
+
+
+
+
+
+    }
+}
